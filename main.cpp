9ffbeb9674367f34a829bf864d5745ff8bd9b3b0
Pecneb <ecneb2000@gmail.com>
2022-09-08 18:17:54 +0200

    TODO: found 1 more error to fix

--- main.cpp
+++ main.cpp
@@ -4,29 +4,22 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    // fix typo N_ELEMENTS
-    int *b = new int[NELEMENTS];
-    // fix ; line ending
-    std::cout << '1-100 ertekek duplazasa'
-    // fix for( int i = 1; i <= N_ELEMENTS; i++ )
-    for (int i = 0;)
+    int *b = new int[N_ELEMENTS];
+    // TODO: fix this also, use "" instead of ''
+    std::cout << '1-100 ertekek duplazasa';
+    for( int i = 1; i <= N_ELEMENTS; i++ )
     {
         b[i] = i * 2;
     }
-    // fix for ( int i = 1; i <= N_ELEMENTS; i++ )
-    for (int i = 0; i; i++)
+    for ( int i = 1; i <= N_ELEMENTS; i++ )
     {
-	// fix ; line ending
-        std::cout << "Ertek:"
+        std::cout << "Ertek:";
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    // fix: en adnek neki kezdo erteket, ne legyen benne memoriaszemet --> int atlag = 0;
-    int atlag;
-    // fix: for ( int i = 1; i <= N_ELEMENTS; i++)
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag = 0;
+    for ( int i = 1; i <= N_ELEMENTS; i++)
     {
-	// fix: ; line ending
-        atlag += b[i]
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
